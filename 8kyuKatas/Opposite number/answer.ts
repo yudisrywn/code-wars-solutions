c40c66be0b2a97ec2317f3f5c8dc3478d59a926c
yudisrywn <yudisuryawan7@students.unnes.ac.id>
2026-04-24 06:11:37 +0700

    solve: Opposite number

---
+++ 8kyuKatas/Opposite number/answer.ts
@@ -0,0 +1,12 @@
+/*
+Jadi suruh buat nilai kebalikan dimana misal 34 jadi -34, -14 jadi 14
+
+gampangnya  postifNum x negatif(-1) = negatifNum
+negatifNum x negatif(-1) = positifNum
+*/
+
+export class Kata {
+  static opposite(n: number) {
+    return n * -1;
+  }
+}
---
+++ 8kyuKatas/Opposite number/question.md
@@ -0,0 +1,7 @@
+Very simple, given a number, find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
