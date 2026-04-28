e1002f5e611dc9cdfb6f49714e6be0cf53434093
yudisrywn <yudisuryawan7@students.unnes.ac.id>
2026-04-28 08:11:27 +0700

    solve: Function 1 - hello world

---
+++ 8kyuKatas/Function 1 - hello world/answer.ts
@@ -0,0 +1,5 @@
+// Export a function "greet" that returns "hello world!"
+// Make a simple function called greet that returns the most-famous "hello world!".
+export function greet() {
+  return "hello world!";
+}
